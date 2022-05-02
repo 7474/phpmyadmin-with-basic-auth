55356b991804782fd58a30985a7461bbf7c34b62
koudenpa <koudenpa@hotmail.com>
2022-05-03 04:12:04 +0900

    Example Terraform use app runner

---
+++ example-terraform/phpmyadmin.tf
@@ -0,0 +1,20 @@
+resource "aws_apprunner_service" "phpmyadmin" {
+  service_name = var.name
+
+  source_configuration {
+    # App Runner doesn't support automatic deployments for an image in an ECR Public repository.
+    auto_deployments_enabled = false
+    image_repository {
+      image_configuration {
+        port = "80"
+        runtime_environment_variables = {
+          PMA_ARBITRARY       = "1"
+          BACIC_AUTH_USER     = var.basic_auth_user
+          BACIC_AUTH_PASSWORD = var.basic_auth_password
+        }
+      }
+      image_identifier      = "public.ecr.aws/g2g6t4l2/phpmyadmin-with-basic-auth-repo:master"
+      image_repository_type = "ECR_PUBLIC"
+    }
+  }
+}
---
+++ example-terraform/provider.tf
@@ -0,0 +1,12 @@
+
+provider "aws" {
+  region     = "ap-northeast-1"
+  access_key = var.access_key
+  secret_key = var.secret_key
+
+  default_tags {
+    tags = {
+      Environment = "phpmyadmin-with-basic-auth"
+    }
+  }
+}
