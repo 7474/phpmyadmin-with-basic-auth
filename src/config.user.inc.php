eeb467a9005aa24e73a5b8d141a17bbca62d33fd
7474 <koudenpa@hatena.ne.jp>
2023-06-05 20:58:48 +0900

    SSL/TLS接続する市内の設定を外に出す。

--- src/config.user.inc.php
+++ src/config.user.inc.php
@@ -1,6 +1,8 @@
 <?php
 // for PlanetScale
-for ($i = 1; isset($hosts[$i - 1]); $i++) {
-    $cfg['Servers'][$i]['ssl_ca'] = '/etc/ssl/certs/ca-certificates.crt';
-    $cfg['Servers'][$i]['ssl'] = true;
+if ($_ENV['PMA_SSL']) {
+    for ($i = 1; isset($hosts[$i - 1]); $i++) {
+        $cfg['Servers'][$i]['ssl_ca'] = '/etc/ssl/certs/ca-certificates.crt';
+        $cfg['Servers'][$i]['ssl'] = true;
+    }
 }
